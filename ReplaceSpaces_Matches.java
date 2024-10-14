7b284f810691aea608035d30b4e6ddbe1bff548e
pankaj571991 <pankazgupta5.com>
2024-10-14 19:47:38 +0530

    14 Oct 2024

---
+++ ReplaceSpaces_Matches.java
@@ -0,0 +1,16 @@
+package basicsprograms;
+
+public class ReplaceSpaces_Matches 
+{
+
+	public static void main(String[] args) 
+	{
+		//Assignment - Replace all spaces with underscore "_"?
+		String name="I am a boy";
+		String n1=name.replaceAll(" ", "_");		
+		System.out.println(n1);
+		
+		
+	}
+
+}
